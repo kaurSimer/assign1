6f078ae0ae473f70380a17a445ac40ea207d6ce3
Simer <missstudy2712@gmail.com>
2024-05-16 11:23:30 -0400

    commit

---
+++ my-react-app/src/component/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+const Navbar = () => {
+    return(
+       
+    <nav>
+ <h1>navbarrr</h1>
+        <ul>
+            <li>
+<link to="/">Home</link>
+{/* <a href="/">Home</a> */}
+            </li>
+        </ul>
+    </nav>
+    )
+}
+export default Navbar;
