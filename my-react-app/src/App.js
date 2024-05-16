efe29c7c7cd55ed6755e03a028b2cab1371a432c
Simer <missstudy2712@gmail.com>
2024-05-16 12:27:45 -0400

    second commit

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -2,14 +2,22 @@
 import './App.css';
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import Navbar from './component/Navbar';
-import Home from './component/Home';
+import Contact from './component/Contact';
+import Services from './component/Services';
+import Privacy from './component/Privacy';
+import About from './component/About';
+
 function App() {
   return (
   
     <Router>
       
       <Routes>
-        <Route path="/" element={<Home/>} />
+        <Route path="/" element={<Navbar/>} />
+<Route path="/contact" element={<Contact/>}/>
+<Route path="/services" element={<Services/>}/>
+<Route path="/about" element={<About/>}/>
+<Route path="/privacy" element={<Privacy/>}/>
 
       </Routes>
     </Router>
--- my-react-app/src/component/Navbar.jsx
+++ my-react-app/src/component/Navbar.jsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+
+// import styled from 'styled-components';
 const Navbar = () => {
     return(
-       
-    <nav>
- <h1>navbarrr</h1>
+   
+    <nav className="navbar">
+<div>
         <ul>
-            <li>
-<link to="/">Home</link>
-{/* <a href="/">Home</a> */}
+            <li className='nav-item'>
+
+            <Link className="nav-link" to="/">Home</Link></li>
+            <li className='nav-item'> <Link className="nav-link" to="/contact">Contact</Link></li>
+            <li className='nav-item'> <Link className="nav-link"  to="/privacy">Privacy</Link></li>
+            <li className='nav-item'> <Link className="nav-link" to="/services">Services</Link></li>
+            <li className='nav-item'>  <Link className="nav-link" to="/about">About</Link>
+
             </li>
         </ul>
+        </div>
     </nav>
-    )
+   
+    );
+};
+const styles = `
+.navbar{
+    background-color: #333;
+        color: #fff;
+        padding: 10px 20px;
 }
+.nav-links {
+    list-style-type: none;
+    margin: 0;
+    padding: 0;
+    display: flex;
+}
+
+.nav-item {
+    margin-right: 20px;
+}
+
+.nav-link {
+    text-decoration: none;
+    color: #fff;
+    font-weight: bold;
+}
+
+.nav-link:hover {
+    color: #ffd700; /* Change color on hover */
+}
+`
+const styleElement = document.createElement('style');
+styleElement.appendChild(document.createTextNode(styles));
+document.head.appendChild(styleElement);
 export default Navbar;
+
